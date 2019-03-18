902d280601de99bbf73e9437ca84fed616e1192d
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-03-18 15:40:47 -0600

    topUserList css

--- client/src/components/PeakList.js
+++ client/src/components/PeakList.js
@@ -24,6 +24,27 @@ export default class PeakList extends Component {
             rank: 48,
             imgUrl:
                "https://images.fineartamerica.com/images-medium-large-5/colorado-14er-mt-eolus-and-the-sidewalk-in-the-sky-aaron-spong.jpg"
+         },
+         {
+            name: "Handies Peak",
+            elev: "14,050",
+            rank: 50,
+            imgUrl:
+               "https://i.pinimg.com/originals/bb/f9/9f/bbf99f5208382128ad41da4c32758801.jpg"
+         },
+         {
+            name: "Mount Wilson",
+            elev: "14,100",
+            rank: 25,
+            imgUrl:
+               "https://www.wildernessshots.com/wp-content/uploads/2017/10/Mt-Wilson-Fall-Colors-Sunrise-Colorado.jpg"
+         },
+         {
+            name: "Mt. Eolus",
+            elev: "14,75",
+            rank: 48,
+            imgUrl:
+               "https://images.fineartamerica.com/images-medium-large-5/colorado-14er-mt-eolus-and-the-sidewalk-in-the-sky-aaron-spong.jpg"
          }
       ]
    };
@@ -32,6 +53,7 @@ export default class PeakList extends Component {
       const { peaks } = this.state;
       return (
          <Container className="peak-list-container">
+            <h2>The Peaks</h2>
             {peaks.map(({ name, elev, rank, imgUrl }) => (
                <ListGroup>
                   <ListGroupItem className="peak-card">
--- client/src/components/TopUserList.js
+++ client/src/components/TopUserList.js
@@ -1,11 +1,48 @@
 import React, { Component } from "react";
+import { ListGroup, ListGroupItem, Button, Container } from "reactstrap";
 
 export default class TopUserList extends Component {
+   state = {
+      users: [
+         {
+            name: "Dehn",
+            totalPeaks: 1,
+            dateJoined: "",
+            peaksClimbed: [],
+            toDoList: []
+         },
+         {
+            name: "Lynette",
+            totalPeaks: 25,
+            dateJoined: "",
+            peaksClimbed: [],
+            toDoList: []
+         },
+         {
+            name: "Brody",
+            totalPeaks: 54,
+            dateJoined: "",
+            peaksClimbed: [],
+            toDoList: []
+         }
+      ]
+   };
+
    render() {
+      const { users } = this.state;
       return (
-         <div>
-            <h3>Recent Users</h3>
-         </div>
+         <Container className="top-user-list">
+            <h3>Top Users:</h3>
+            {users.map(({ name, totalPeaks }) => (
+               <ListGroup>
+                  <ListGroupItem className="user-card">
+                     <h5>{name}</h5>
+                     <p>Peaks Climbed: {totalPeaks}</p>
+                     <Button color="success">Profile</Button>
+                  </ListGroupItem>
+               </ListGroup>
+            ))}
+         </Container>
       );
    }
 }
