bdf61fade5e632f450f77177b93ef599baa07845
Dehn Hunsworth <dhunsworth@yahoo.com>
2019-04-26 12:26:00 -0600

    heroku scripts

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/peaks", peaks);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.Port || 5000;
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
